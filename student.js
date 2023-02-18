e7e333d66a0c3a2a0d531463e639a267765a513c
ahsanzubair00 <ahsan.zubair@devsinc.com>
2023-02-18 19:30:50 +0500

    get

---
+++ student.js
@@ -0,0 +1,59 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'eu-west-2'}); // replace with your desired region
+
+const dynamodb = new AWS.DynamoDB();
+
+const params = {
+  TableName: "Students",
+  KeySchema: [
+    { AttributeName: "name", KeyType: "HASH" },  // Partition key
+    { AttributeName: "class", KeyType: "RANGE" }  // Sort key
+  ],
+  AttributeDefinitions: [
+    { AttributeName: "name", AttributeType: "S" },
+    { AttributeName: "class", AttributeType: "S" }
+  ],
+  ProvisionedThroughput: {
+    ReadCapacityUnits: 5,
+    WriteCapacityUnits: 5
+  }
+};
+ const create = async () => {
+  dynamodb.createTable(params, function(err, data) {
+    if (err) {
+      console.error("Error creating table: ", err);
+    } else {
+      console.log("Table created successfully: ", data);
+    }
+  });
+
+ }
+
+ await create();
+
+ const docClient = new AWS.DynamoDB.DocumentClient();
+
+const paramss = {
+  TableName: "Students",
+  Item: {
+    "name": "John Doe",
+    "class": "History"
+  }
+};
+const put = async () => {
+  docClient.put(paramss, function(err, data) {
+    if (err) {
+      console.error("Error adding item: ", err);
+    } else {
+      console.log("Item added successfully: ", data);
+    }
+  });
+  
+}
+
+await put()
+
+
+
+
+
